5970a6976606ed0b8eee05b0f31930dca2f8127d
Satyan Gupta <satyangupta.sg@gmail.com>
2021-01-19 02:28:29 +0530

    queue system

--- speech-recognition.py
+++ speech-recognition.py
@@ -8,7 +8,7 @@ def speech_to_text():
 	# Record Audio
 	r = sr.Recognizer()
 	with sr.Microphone() as source:
-	    print("Say something!")
+	    print("Enter Command")
 	    audio = r.listen(source)
 
 	# Speech recognition using Google Speech Recognition
@@ -24,9 +24,38 @@ def speech_to_text():
 	return r.recognize_google(audio)
 
 
-def enter_floor_number(argument):
-	switcher = {
-	"ground floor": 0,
+# def enter_floor_number(argument):
+# 	switcher = {
+# 	"Ground floor": 0,
+# 	"1st floor": 1,
+# 	"first floor": 1,
+# 	"2nd floor": 2,
+# 	"second floor": 2,
+# 	"3rd floor": 3,
+# 	"third floor": 3,
+# 	"4th floor": 4,
+# 	"fourth floor": 4,
+# 	"5th floor": 5,
+# 	"fifth floor": 5,
+# 	"6th floor": 6,
+# 	"sixth floor": 6,
+# 	"7th floor": 7,
+# 	"seventh floor": 7,
+# 	"8th floor": 8,
+# 	"eighth floor": 8,
+# 	"9th floor": 9,
+# 	"ninth floor": 9,
+# 	"10th floor": 10,
+# 	"tenth floor": 10,
+# 	}	
+
+# 	if switcher.get(argument) == None:
+# 		print("Invalid Input")
+# 	else:
+# 		return switcher[argument]
+
+floors = {
+	"Ground floor": 0,
 	"1st floor": 1,
 	"first floor": 1,
 	"2nd floor": 2,
@@ -46,13 +75,18 @@ def enter_floor_number(argument):
 	"9th floor": 9,
 	"ninth floor": 9,
 	"10th floor": 10,
-	"tenth floor": 10
-	}	
+	"tenth floor": 10,
+}	
 
-	if switcher.get(argument) == None:
-		print("Invalid Input")
-	else:
-		return switcher[argument]
+queue = []
 
+while(1):
+	x = speech_to_text()
+	if x == 'exit' or x == 'Exit':
+		break
+	elif x in floors:
+		queue.append(enter_floor_number(x))
+	else:	
+		print('Invalid input. Try again.')
 
-print(enter_floor_number(speech_to_text()))
+print(queue)
