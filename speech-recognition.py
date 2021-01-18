3bffc8e2047e8397b8dce5af06e9ed9891700f23
Satyan Gupta <satyangupta.sg@gmail.com>
2021-01-18 12:05:20 +0530

    Floor number

--- speech-recognition.py
+++ speech-recognition.py
@@ -14,7 +14,7 @@ try:
     # for testing purposes, we're just using the default API key
     # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
     # instead of `r.recognize_google(audio)`
-    print("You said: " + r.recognize_google(audio))
+    print("Enter Floor Number: " + r.recognize_google(audio))
 except sr.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
