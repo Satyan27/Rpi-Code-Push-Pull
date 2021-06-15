70ea1e5a9e503a4d9948ef04ab93068f5434aee9
Satyan Gupta <satyangupta.sg@gmail.com>
2021-06-15 15:26:15 +0530

    update

--- proxy.py
+++ proxy.py
@@ -7,29 +7,29 @@ ECHO = 24
 
 print "Distance Measurement In Progress"
 
-GPIO.setup(TRIG,GPIO.OUT)
-GPIO.setup(ECHO,GPIO.IN)
+while(1):
+  GPIO.setup(TRIG,GPIO.OUT)
+  GPIO.setup(ECHO,GPIO.IN)
 
-GPIO.output(TRIG, False)
-print "Waiting For Sensor To Settle"
-time.sleep(2)
+  GPIO.output(TRIG, False)
+  print "Waiting For Sensor To Settle"
+  time.sleep(2)
 
-GPIO.output(TRIG, True)
-time.sleep(0.00001)
-GPIO.output(TRIG, False)
-try:
-  while (1):
-    while GPIO.input(ECHO)==0:
-      pulse_start = time.time()
+  GPIO.output(TRIG, True)
+  time.sleep(0.00001)
+  GPIO.output(TRIG, False)
 
-    while GPIO.input(ECHO)==1:
-      pulse_end = time.time() 
+  while GPIO.input(ECHO)==0:
+    pulse_start = time.time()
 
-    pulse_duration = pulse_end - pulse_start
-    distance = pulse_duration * 17150
-    distance = round(distance, 2)
+  while GPIO.input(ECHO)==1:
+    pulse_end = time.time() 
 
-    if (distance < 15):
-      print "Distance:",distance,"cm"
-except:
+  pulse_duration = pulse_end - pulse_start
+  distance = pulse_duration * 17150
+  distance = round(distance, 2)
+
+  if (distance < 15.0):
+    print "Distance:",distance,"cm"
+
   GPIO.cleanup()
